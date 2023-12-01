80639c8b14c8fbce6755f3d516aedacebb527231
Mark Philpot <mark.philpot@gmail.com>
2023-11-30 22:43:36 -0800

    Formatting

--- common/dayTemplate/src/test/java/com/markphilpot/CalibrationTest.java
+++ common/dayTemplate/src/test/java/com/markphilpot/CalibrationTest.java
@@ -3,13 +3,12 @@ package com.markphilpot;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.junit.Test;
-
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.Scanner;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Test;
 
 public class CalibrationTest {
   private static final Logger log = LogManager.getLogger(CalibrationTest.class);
@@ -18,19 +17,21 @@ public class CalibrationTest {
   public void testSamplePart1() {
     var inputStream = CalibrationTest.class.getClassLoader().getResourceAsStream("sample.txt");
 
-    var inputs = Map.of(
+    var inputs =
+        Map.of(
             "1abc2", 12,
             "pqr3stu8vwx", 38,
             "a1b2c3d4e5f", 15,
-            "treb7uchet", 77
-    );
+            "treb7uchet", 77);
 
-    inputs.forEach((line, result) -> {
-      var value = Calibration.getCalibrationValue(LineParsing.lineToList(line));
-      assertThat(value, is(result));
-    });
+    inputs.forEach(
+        (line, result) -> {
+          var value = Calibration.getCalibrationValue(LineParsing.lineToList(line));
+          assertThat(value, is(result));
+        });
 
-    var total = inputs.entrySet().stream()
+    var total =
+        inputs.entrySet().stream()
             .map(entry -> Calibration.getCalibrationValue(LineParsing.lineToList(entry.getKey())))
             .reduce(0, Integer::sum);
 
@@ -45,36 +46,39 @@ public class CalibrationTest {
 
     var scanner = new Scanner(inputStream).useDelimiter("\n");
 
-    while(scanner.hasNext()) {
+    while (scanner.hasNext()) {
       inputs.add(scanner.next());
     }
 
-    var total = inputs.stream()
+    var total =
+        inputs.stream()
             .map(entry -> Calibration.getCalibrationValue(LineParsing.lineToList(entry)))
             .reduce(0, Integer::sum);
 
     log.info(total);
-//    assertThat(total, is(0));
+    //    assertThat(total, is(0));
   }
 
   @Test
   public void testSamplePart2() {
-    var inputs = Map.of(
+    var inputs =
+        Map.of(
             "two1nine", 29,
             "eightwothree", 83,
             "abcone2threexyz", 13,
             "xtwone3four", 24,
             "4nineeightseven2", 42,
             "zoneight234", 14,
-            "7pqrstsixteen", 76
-    );
+            "7pqrstsixteen", 76);
 
-    inputs.forEach((line, result) -> {
-      var value = Calibration.getCalibrationValueWithWords(line);
-      assertThat(value, is(result));
-    });
+    inputs.forEach(
+        (line, result) -> {
+          var value = Calibration.getCalibrationValueWithWords(line);
+          assertThat(value, is(result));
+        });
 
-    var total = inputs.entrySet().stream()
+    var total =
+        inputs.entrySet().stream()
             .map(entry -> Calibration.getCalibrationValueWithWords(entry.getKey()))
             .reduce(0, Integer::sum);
 
@@ -94,13 +98,12 @@ public class CalibrationTest {
 
     var scanner = new Scanner(inputStream).useDelimiter("\n");
 
-    while(scanner.hasNext()) {
+    while (scanner.hasNext()) {
       inputs.add(scanner.next());
     }
 
-    var total = inputs.stream()
-            .map(Calibration::getCalibrationValueWithWords)
-            .reduce(0, Integer::sum);
+    var total =
+        inputs.stream().map(Calibration::getCalibrationValueWithWords).reduce(0, Integer::sum);
 
     log.info(total);
   }
--- common/utils/src/main/java/com/markphilpot/StringUtils.java
+++ common/utils/src/main/java/com/markphilpot/StringUtils.java
@@ -1,20 +1,17 @@
 package com.markphilpot;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class StringUtils {
-    private static final Logger log = LogManager.getLogger(StringUtils.class);
+  private static final Logger log = LogManager.getLogger(StringUtils.class);
 
-    public static <T extends Object> List<T> reverse(List<T> input) {
-        var rList = new ArrayList<T>();
-        new LinkedList<>(input).
-                descendingIterator().
-                forEachRemaining(rList::add);
-        return rList;
-    }
+  public static <T extends Object> List<T> reverse(List<T> input) {
+    var rList = new ArrayList<T>();
+    new LinkedList<>(input).descendingIterator().forEachRemaining(rList::add);
+    return rList;
+  }
 }
